22977b5c9c24370625a263ef84d3c1ff9c095042
josemnr <maninari97@gmail.com>
2021-03-18 10:06:30 -0600

    Fix sendData method to fetch correctly

--- src/controllers/home_page.js
+++ src/controllers/home_page.js
@@ -1,39 +1,33 @@
 const {config} = require('../../config');
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const fetch = require('node-fetch');
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: config.apikey,
-  }),
-  serviceUrl: config.serviceUrl,
-});
-
-//Método para render de la página del Adder
+//Método para render de HomePage
 exports.renderHomePage = (req, res) => {
     res.render("index");
 }
 
 exports.sendData = (req, res) => {
-    console.log(req.body.entry)
-    let text = req.body.entry
-    const toneParams = {
-        toneInput: { 'text': text },
-        contentType: 'application/json',
-    };
-      
-    toneAnalyzer.tone(toneParams)
-    .then(toneAnalysis => {
-    //   console.log(JSON.stringify(toneAnalysis, null, 2));
-        res.render("index", {
-            response:JSON.stringify(toneAnalysis, null, 2)
-        });
-    })
-    .catch(err => {
-        console.log('error:', err);
-        res.render("index", {
-            response:JSON.stringify(err, null, 2)
-        });
+    let text = req.body.entry;
+    let data = {entry: text};
+
+    fetch(config.cloudFoundryUrl, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-Type': 'application/json'
+    }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+        console.log('Success:', response)
+        if(response.result){
+            res.render("index", {
+                response: JSON.stringify(response.result)
+            });
+        }else{
+            res.render("index", {
+                response: JSON.stringify(response)
+            });
+        }
     });
 }
