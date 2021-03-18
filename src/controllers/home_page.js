aa15799da19eff5c49e9bf454896ed1df631fc30
josemnr <maninari97@gmail.com>
2021-03-17 20:54:06 -0600

    Update home_page controller

--- src/controllers/home_page.js
+++ src/controllers/home_page.js
@@ -3,7 +3,7 @@ const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const toneAnalyzer = new ToneAnalyzerV3({
-  version: '{version}',
+  version: '2017-09-21',
   authenticator: new IamAuthenticator({
     apikey: config.apikey,
   }),
@@ -16,8 +16,22 @@ exports.renderHomePage = (req, res) => {
 }
 
 exports.sendData = (req, res) => {
-    console.log(req.body)
+    console.log(req.body.entry)
+    let text = req.body.entry
+    const toneParams = {
+        toneInput: { 'text': text },
+        contentType: 'application/json',
+      };
+      
+      toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+          console.log(JSON.stringify(toneAnalysis, null, 2));
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
+
     res.render("index", {
-        response:req.body.entry
+        text
     });
 }
