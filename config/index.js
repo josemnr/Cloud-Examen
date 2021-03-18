68bd684031aa5378f5bc3e49ce50789ad20452ad
josemnr <maninari97@gmail.com>
2021-03-18 09:13:57 -0600

    Update config

--- config/index.js
+++ config/index.js
@@ -4,8 +4,7 @@ const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 8080,
     host: process.env.HOST || "0.0.0.0",
-    apikey: process.env.TONE_ANALYZER_HP_APIKEY,
-    serviceUrl: process.env.TONE_ANALYZER_HP_URL
+    cloudFoundryUrl: process.env.ClOUD_FOUNDRY_URL
 }
 
 module.exports = { config };
