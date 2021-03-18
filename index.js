af802e31a1772c4df255040dd0fd553777b39bac
josemnr <maninari97@gmail.com>
2021-03-17 22:04:41 -0600

    Add path to implement static files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const {config} = require('./config');
 const homePageRouter = require('./src/routes/home_page');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, './public')));
 app.set("views", "views");
 app.set("view engine", "hbs");
 
